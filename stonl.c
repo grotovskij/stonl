2b5d9999bd465fce69aedd5208ebbd93fb68f473
Mikhail <56311832+grotovskij@users.noreply.github.com>
2021-02-07 13:45:15 +0400

    Update stonl.c

--- stonl.c
+++ stonl.c
@@ -8,23 +8,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+
+int getFileSize(const char *fileName)
+{
+    struct stat file_stat;
+
+    stat(fileName, &file_stat);
+    return file_stat.st_size;
+}
 
 int main(int argc, char* argv[])
 {
 	FILE *target = fopen(argv[1], "r");
-	int counter = 0;
-	char fileContent[100];
+	int counter;
+
+	if(argc < 1)
+	{
+		printf("No arguments given.\nUsage: stonl <FILE> [NUMBER OF SPACES TO REPLACE]\n");
+		return 1;
+	}
 
 	if(target == NULL)
 	{
-		printf("No such file.\nUsage: stonl <FILE> [NUMBER OF SPACES]\n");
+		printf("No such file.\nUsage: stonl <FILE> [NUMBER OF SPACES TO REPLACE]\n");
 
 		return 1;
 	}
 
-	fgets(fileContent, 100, target);
-
 	int newStringsAmount = argc == 3 ? atoi(argv[2]) : 0;
+
+	char *fileContent = malloc(getFileSize(argv[1]));
+
+	for(counter = 0; counter < getFileSize(argv[1]); ++counter)
+		fileContent[counter] = (char)fgetc(target);
 
 	for(counter = 0; newStringsAmount >= 1 && fileContent[counter] != 0; ++counter)
 		if(fileContent[counter] == ' ')
@@ -35,7 +52,7 @@ int main(int argc, char* argv[])
 			fileContent[counter] = '\n';
 
 	printf("%s\n", fileContent);
-
+	free(fileContent);
 	fclose(target);
 
 	return 0;
